06f1d76847ef3571ace33d4a4e1cc835965bfb75
Nensin <pereznensin@gmail.com>
2026-03-30 11:27:18 -0400

    Funcion para el modal Molaridad y cambio en los nombres de los field de molaridad

--- JS/funcionamiento.js
+++ JS/funcionamiento.js
@@ -12,22 +12,45 @@ document.querySelectorAll('.overlay').forEach(overlay =>{
         if(e.target===overlay){
             overlay.classList.remove('active');
             document.body.style.overflow = '';
-
         }
     });
 });
+//Permite que con enter se active el boton de calcular
+document.addEventListener('keydown', (e) =>{
+    if(e.key === 'Enter'){
+        const modalAbierto = document.querySelector('.overlay.active');
+        if(!modalAbierto) return;
+
+        const boton = modalAbierto.querySelector('.btn-calc:last-of-type');
+        boton.click();
+    }
+});
+
 //Funcion para mostrar los resultados
 function showResult(boxId, valueId, value){
     document.getElementById(valueId).textContent=parseFloat(value.toFixed(4))
     document.getElementById(boxId).classList.add('visible')
 }
+//Funcion Calcular los moles
 function calcMoles(){
     const masa = parseFloat(document.getElementById('moles-masa').value);
     const mmolar =parseFloat(document.getElementById('moles-mmolar').value);
     //Validacion
     if(isNaN(masa) || isNaN(mmolar)){
         alert('Por favor ingresa todos los valores.')
+        return;
     }
     let resultado = masa/mmolar;
     showResult('moles-result', 'moles-value', resultado);
+}
+//Funcion para Calcular la Molaridad
+function calcMolaridad(){
+    const moles =parseFloat( document.getElementById('mola-moles').value);
+    const litros = parseFloat(document.getElementById('mola-vol').value);
+     if(isNaN(moles) || isNaN(litros)){
+        alert('Por favor ingrese todos los valores.')
+        return;
+     }
+    let result = moles/litros;
+    showResult('molaridad-result','molaridad-value',result)
 }
