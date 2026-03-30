d4467b92761e8a39a7f07000f1c06434079d4876
Nensin <pereznensin@gmail.com>
2026-03-30 10:39:45 -0400

    Se agego para abrir y cerrar los modales

---
+++ JS/funcionamiento.js
@@ -0,0 +1,16 @@
+//Modal:Open y Close
+function openModal(id){
+    document.getElementById('modal-' + id).classList.add('active');
+    document.body.style.overflow = 'hidden';
+}
+function closeModal(id){
+    document.getElementById('modal-' + id).classList.remove('active')
+    document.body.style.overflow = 'hidden';
+}
+document.querySelectorAll('.overlay').forEach(overlay =>{
+    overlay.addEventListener('click', (e) =>{
+        if(e.target===overlay){
+            overlay.classList.remove('active');
+        }
+    });
+});
