2ec738b894b9a5f2575317fdc5712235a11d8dc5
Nensin <pereznensin@gmail.com>
2026-03-31 00:00:46 -0400

    Se cambio un simbolo en el idex. Se agrego color al scroll del Css y se añadio todo el funcionamiento para formula empirica. Falta la validacion

--- JS/funcionamiento.js
+++ JS/funcionamiento.js
@@ -2,10 +2,17 @@
 function openModal(id){
     document.getElementById('modal-' + id).classList.add('active');
     document.body.style.overflow = 'hidden';
+
+    //Crea los field para Mm y Fe
     if(id === 'masa-molar'){
         const container = document.getElementById('mm-elementos');
         container.innerHTML = ''; // limpia los campos anteriores
         addElementoMm();          // agrega uno fresco
+    }
+    if(id === 'empirica'){
+        const container = document.getElementById('fe-elementos');
+        container.innerHTML = ''; // limpia los campos anteriores
+        addElementofe();          // agrega uno fresco
     }
 }
 function closeModal(id){
@@ -20,6 +27,8 @@ document.querySelectorAll('.overlay').forEach(overlay =>{
         }
     });
 });
+/*--------------------------------*/
+
 //Permite que con enter se active el boton de calcular
 document.addEventListener('keydown', (e) =>{
     if(e.key === 'Enter'){
@@ -30,12 +39,15 @@ document.addEventListener('keydown', (e) =>{
         boton.click();
     }
 });
+/*--------------------------------*/
 
 //Funcion para mostrar los resultados
 function showResult(boxId, valueId, value){
     document.getElementById(valueId).textContent=parseFloat(value.toFixed(8))
     document.getElementById(boxId).classList.add('visible')
 }
+/*--------------------------------*/
+
 //Funcion Calcular los moles
 function calcMoles(){
     const masa = parseFloat(document.getElementById('moles-masa').value);
@@ -173,6 +185,8 @@ function calcMasaMolar(){
     showResult('masa-molar-result','masa-molar-value', total);
 
 }
+/*--------------------------------*/
+
 //Funcion Porciento de masa
 function calcPorciento(){
     const Masaelemento = parseFloat(document.getElementById('pct-elemento').value);
@@ -185,4 +199,87 @@ function calcPorciento(){
     //Procedimiento
     let result = (Masaelemento/MmolarComp) * 100;
     showResult('porciento-result','porciento-value',result)
+}
+//Funcion Formula Empirica: crear los elementos
+function addElementofe(){
+    const container = document.getElementById('fe-elementos');
+    const div = document.createElement('div');
+    div.style.cssText = 'display:grid; grid-template-columns:1fr 1fr; gap:10px; margin-bottom:12px;';
+
+    div.innerHTML = `
+    <div class="field">
+        <label>Símbolo</label>
+        <input type="text" class="fe-simbolo" placeholder="ej. N" maxlength="2"/>
+    </div>
+    <div class ="field">
+        <label>Gramos del elemento</label>
+        <input type="number" step="any" class="cantidad-g" placeholder="ej. 0.483g N" min="0"/>
+    </div>
+    <div class="field">
+        <label>Masa atomica del elem.</label>
+        <input type="number" step="any" class="masa-atomica" placeholder="ej. 14g N" min="1"/>
+    </div>
+    <button class="btn-eliminar" onclick="this.parentElement.remove()">✕</button>
+    `;
+    container.appendChild(div);
+}
+//Funcion para calcular los enteros
+function aEnteros(ratios){
+   /*es lo mismo que: 
+    let multiplicados = [];
+    for(let i = 0; i < ratios.length; i++){
+    multiplicados.push(ratios[i] * factor);}
+*/
+    for(let factor = 1; factor <=8; factor++){
+        const multiplicados = ratios.map(x => x * factor);
+
+        const todosEnteros = multiplicados.every(n => Math.abs(n - Math.round(n)) < 0.08);
+        if(todosEnteros){
+            return multiplicados.map(n=> Math.round(n));
+        }
+    }
+    return ratios.map(r => Math.round(r));
+}
+//Funcion: Formula Empirica
+function calcEmpirica(){
+    const cantidades=document.querySelectorAll('.cantidad-g');
+    const matomica=document.querySelectorAll('.masa-atomica');
+    
+
+    //Array que guarda los valores en el orden que fueron entrados
+    let moles = [];
+    //bucle que va dividiendo para hallay los moles
+    for(let i = 0; i<cantidades.length; i++){
+        const gramos =parseFloat(cantidades[i].value);
+        const masa =parseFloat(matomica[i].value);
+        moles.push(gramos/masa);
+    }
+    
+    //busca el valor mas pequeño en el array moles
+    const molMin = Math.min(...moles);
+    
+    //Array que guardara la division de todos por el minimo
+    let ratios = [];
+    //Bucle que recorre el array moles diviendo por el mol mas pequeño y lo almacena en el array ratios
+    for(let i = 0; i < moles.length; i++){
+        ratios.push(moles[i]/molMin);
+    }
+    let enteros =aEnteros(ratios);
+
+    const simbolos = document.querySelectorAll('.fe-simbolo');
+    let formula = '';
+    for(let i = 0; i < enteros.length; i++){
+        const simbolo = simbolos[i].value || '?';
+        const subindice = enteros[i];
+
+        //Subindice == 1 no se escribe
+        if(subindice === 1){
+            formula += simbolo;
+        }else{
+            formula += simbolo + subindice
+        }
+    }
+    //Muestra el resultado
+    document.getElementById('empirica-value').textContent = formula;
+    document.getElementById('empirica-result').classList.add('visible');
 }
